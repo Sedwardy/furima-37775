33a677e08cec4f2170f10c5754f323428c18fd7e
Sedwardy <101158263+Sedwardy@users.noreply.github.com>
2022-04-07 11:47:54 +0900

    userモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :merchandises
+  has_many :purchases
+
+  validates :nickname,           presence: true
+  validates :last_name,          presence: true
+  validates :first_name,         presence: true  
+  validates :hurigana_last_name, presence: true
+  validates :hurigana_last_name, presence: true
+  validates :date_of_birth,      presence: true
 end
+
+
+
+  
