2a597071ee4445de565983e7eccad6106d9e8030
Sedwardy <101158263+Sedwardy@users.noreply.github.com>
2022-04-13 14:39:31 +0900

    Active Hashの実装

--- app/controllers/merchandises_controller.rb
+++ app/controllers/merchandises_controller.rb
@@ -1,4 +1,8 @@
 class MerchandisesController < ApplicationController
+  before_action :set_merchandise, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
   def index
   end
 
@@ -25,5 +29,19 @@ class MerchandisesController < ApplicationController
   end
   
   def destroy
+  end
+
+  private
+
+  def merchandise_params
+    params.require(:merchandise).permit(:goods_name, :price, :prefecture_id, :explain, :category_id, :condition_id, :delivery_fee_id, :date_of_shipping_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @merchandise = Merchandise.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @merchandise.user
   end
 end
--- app/models/merchandise.rb
+++ app/models/merchandise.rb
@@ -8,11 +8,21 @@ class Merchandise < ApplicationRecord
   has_one_attached :image
 
   validates :goods_name,          null: false 
-  validates :price,               null: false 
+    with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    presence: { message: "can't be blank"}
+  end
   validates :prefecture_id,       null: false 
   validates :explain,             null: false 
   validates :category_id,         null: false 
   validates :condition_id,        null: false 
   validates :delivery_fee_id,     null: false 
   validates :date_of_shipping_id, null: false 
+
+  validates :goods_name, :explain, :image, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }  
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :date_of_shipping_id, numericality: { other_than: 1 } 
 end
