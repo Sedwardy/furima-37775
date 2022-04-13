181926b6d968114b8066503af42a560d8c991865
Sedwardy <101158263+Sedwardy@users.noreply.github.com>
2022-04-13 16:30:21 +0900

    編集画面の実装

--- spec/models/merchandise_spec.rb
+++ spec/models/merchandise_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Merchandise, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @merchandise = FactoryBot.build(:merchandise)
+  end
+
+  describe '商品の出品' do
+    context '商品が出品できる場合' do
+      it "nameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+        expect(@user).to be_valid
+      end
+    end
