3044a905d5f85fe3f45e8f47a6ac9b0b023db88d
Sedwardy <101158263+Sedwardy@users.noreply.github.com>
2022-04-13 18:28:59 +0900

    単体テストコードの実装

--- spec/factories/merchandises.rb
+++ spec/factories/merchandises.rb
@@ -1,5 +1,24 @@
 FactoryBot.define do
   factory :merchandise do
+    id                  {1}
+    user_id             {1}
+    goods_name          {"hoge"}
+    explain             {Faker::Lorem.sentence}
+    condition_id        {2}
+    delivery_fee_id     {2}
+    prefecture_id       {2}
+    date_of_shipping_id {2}
+    price               {"9999"}
+    category_id         {2}
+    created_at          {'2022-04-13'}
+    updated_at          {'2022-04-13'}
     
+    association :user
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/sample1.png'), filename: 'sample1.png')
+    end
+    
+    
+
   end
 end
--- spec/models/merchandise_spec.rb
+++ spec/models/merchandise_spec.rb
@@ -5,9 +5,38 @@ RSpec.describe Merchandise, type: :model do
     @merchandise = FactoryBot.build(:merchandise)
   end
 
-  describe '商品の出品' do
-    context '商品が出品できる場合' do
-      it "nameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
-        expect(@user).to be_valid
+  describe '商品' do
+    context '出品できる場合' do
+      it "ユーザーと画像が存在すれば登録できる" do
+        expect(@merchandise).to be_valid
       end
     end
+
+    context '出品できない場合' do
+      it '商品名が空では登録できない' do
+        @merchandise.goods_name = ''
+        @merchandise.valid?
+        expect(@merchandise.errors.full_messages).to include("Goods name can't be blank")
+      end
+
+      it '商品の説明が空では登録できない' do
+        @merchandise.explain = ''
+        @merchandise.valid?
+        expect(@merchandise.errors.full_messages).to include("Explain can't be blank")
+      end
+
+      it '価格が空では登録できない' do
+        @merchandise.price = ''
+        @merchandise.valid?
+        expect(@merchandise.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '価格が数字でないと登録できない' do
+        @merchandise.price = "ああああああ"
+        @merchandise.valid?
+        expect(@merchandise.errors.full_messages).to include("Price is not a number")
+      end
+
+    end
+  end
+end
