93faeaa065d0aea67ff41688f14932dd6aa8758d
Sedwardy <101158263+Sedwardy@users.noreply.github.com>
2022-04-18 11:28:01 +0900

    商品削除機能の実装

--- app/controllers/merchandises_controller.rb
+++ app/controllers/merchandises_controller.rb
@@ -1,7 +1,7 @@
 class MerchandisesController < ApplicationController
   before_action :set_merchandise, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update] #:destroy
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   
   def index
     @merchandises = Merchandise.order(id: :DESC)
@@ -33,8 +33,10 @@ class MerchandisesController < ApplicationController
     end
   end
   
-  #def destroy
-  #end
+  def destroy
+    @merchandise = Merchandise.find(params[:id])
+    redirect_to root_path
+  end
 
   private
 
